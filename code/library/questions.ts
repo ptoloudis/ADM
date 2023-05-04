4cfcdbbfffe36548f9ab0506fab9ff9ac9df8b4f
panagiotis toloudis <57294317+ptoloudis@users.noreply.github.com>
2023-05-04 13:07:46 +0300

    final

---
+++ code/library/questions.ts
@@ -0,0 +1,88 @@
+export const questionsdrug = [
+    {
+        name: 'drugName',
+        message: 'What is the name of the Drug?',
+    },
+    {
+        name: 'condition',
+        message: 'What is the condition of the Drug?',
+    }
+];
+
+export const questionsprotein =[
+    {
+        name: 'structureId',
+        message: 'What is the structureId of the protein?',
+    },
+    {
+        name: 'classification',
+        message: 'What is the classification of the protein?',
+    },
+    {
+        name: 'experimentalTechnique',
+        message: 'What is the experimentalTechnique of the protein?',
+    },
+    {
+        name: 'macromoleculeType',
+        message: 'What is the macromoleculeType of the protein?',
+    },
+    {
+        name: 'residueCount',
+        message: 'What is the residueCount of the protein?',
+    },
+    {
+        name: 'resolution',
+        message: 'What is the resolution of the protein?',
+    },
+    {
+        name: 'structureMolecularWeight',
+        message: 'What is the structureMolecularWeight of the protein?',  
+    },  
+    {
+        name: 'crystallizationMethod',
+        message: 'What is the crystallizationMethod of the protein?',
+    },
+    {
+        name: 'densityMatthews',
+        message: 'What is the densityMatthews of the protein?',
+    },
+    {
+        name: 'densityPercentSol',
+        message: 'What is the densityPercentSol of the protein?',
+    },
+    {
+        name: 'pdbxDetails',
+        message: 'What is the pdbxDetails of the protein?',
+    },
+    {
+        name: 'phValue',
+        message: 'What is the phValue of the protein?',
+    },
+    {
+        name: 'publicationYear',
+        message: 'What is the publicationYear of the protein?',
+    }
+];
+
+export const questionsproteinsequence =[
+    {
+        name: 'structureId',
+        message: 'What is the structureId of the protein?',
+    },
+    {
+        name: 'chainId',
+        message: 'What is the chainId of the protein?'
+    },
+    {
+        name: 'sequence',
+        message: 'What is the Sequence of the protein?'
+    },
+    {
+        name: 'residueCount',
+        message: 'What is the residueCount of the protein?'
+    },
+    {
+        name: 'macromoleculeType',
+        message: 'What is the macromoleculeType of the protein?'
+    }
+];
